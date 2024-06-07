2dfd5eb4903821fbb3e429e7aca5c251af7c309b
Zonia Ramirez <jslnramirez@gmail.com>
2024-06-06 20:31:32 -0400

    updated html and js

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
 const searchForm = document.querySelector('#search-form');
 const qInput = document.querySelector('#q');
-
-fetch('https://www.loc.gov/search/?q=baseball&fo=json')
-.then(function (response) {
-    return response.json();
-})
-    .then(function (data) {
-        console.log('SEARCH')
-        console.log(data);S
-    })
-    .catch(function (err) {
-        console.log(err);
-    });
+const resultList = document.querySelector('#results');
 
 fetch('https://www.loc.gov/film-and-videos/?q=dog&fo=json')
 .then(function (response) {
@@ -37,10 +26,21 @@ fetch('https://www.loc.gov/film-and-videos/?q=dog&fo=json')
     .then(function (data) {
         console.log('SEARCH')
         console.log(data);
+
+        for (let result of data.results) {
+            const columnEl = document.createElement('div');
+            const cardEL = document.createElement('div');
+            
+            columnEl.className = 'col-12';
+            cardEL.className = 'card p-3 m-3';
+            cardEL.textContent = result.title;
+
+            columnEl.appendChild(cardEL);
+
+            resultList.appendChild(columnEl);
+        }
     })
     .catch(function (err) {
         console.log(err);
-    });S
-
-
-    })
+    });
+ });
